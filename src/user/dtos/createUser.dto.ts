90510d81f476d11bd0f172c357b87936679499c1
Bruno Mano <brunomano2004@gmail.com>
2024-12-27 14:40:25 -0300

    create user post

---
+++ src/user/dtos/createUser.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUser {
+  name: string;
+  email: string;
+  phone: string;
+  cpf: string;
+  password: string;
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUser } from './dtos/createUser.dto';
+
+@Controller('user')
+export class UserController {
+  @Post()
+  async createUser(@Body() createUser: CreateUser) {
+    return {
+      ...createUser,
+      password: undefined,
+    };
+  }
+}
